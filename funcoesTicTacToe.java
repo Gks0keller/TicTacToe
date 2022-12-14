b0e4e79037978da6b6102c29452de5dd2178e586
GABRIEL KOEHLER DA SILVA <gabriel_k_da-silva@estudante.sesisenai.org.br>
2022-12-14 18:28:37 -0300

    arquivos finais

--- funcoesTicTacToe.java
+++ funcoesTicTacToe.java
@@ -1,164 +1,42 @@
+import javax.swing.JOptionPane;
 
 public class funcoesTicTacToe{
 
-     static void tabela(char[][] jogoDaVelha){
-        System.out
-            .println("       " + jogoDaVelha[0][0] + " | " + jogoDaVelha[0][1] + " | " + jogoDaVelha[0][2]+ " \n" +
-                     "      -----------\n" +
-                     "       " + jogoDaVelha[1][0] + " | " + jogoDaVelha[1][1] + " | " + jogoDaVelha[1][2]+ " \n" +
-                     "      -----------\n" +
-                     "       " + jogoDaVelha[2][0] + " | " + jogoDaVelha[2][1] + " | " + jogoDaVelha[2][2]+ " \n");
+     public static void tabela(char[][] jogoDaVelha){
+        JOptionPane.showMessageDialog(null,
+                    "                 " + jogoDaVelha[0][0] + "  |  " + jogoDaVelha[0][1] + "  |  " + jogoDaVelha[0][2]+ " \n" +
+                    "                 -----------\n" +
+                    "                 " + jogoDaVelha[1][0] + "  |  " + jogoDaVelha[1][1] + "  |  " + jogoDaVelha[1][2]+ " \n" +
+                    "                 -----------\n" +
+                    "                 " + jogoDaVelha[2][0] + "  |  " + jogoDaVelha[2][1] + "  |  " + jogoDaVelha[2][2]+ " \n");
+
     }
-    public static char[][] l1(int lugar1,char[][] jogoDaVelha){
-        
-        if (lugar1 == 7) {
-            if(jogoDaVelha[0][0]=='-'){
-                jogoDaVelha[0][0] = 'o';
-            }       
-        } else if (lugar1 == 8) {
-            if(jogoDaVelha[0][1]=='-'){
-
-                jogoDaVelha[0][1] = 'o';
-            }
-        } else if (lugar1 == 9) {
-            if(jogoDaVelha[0][2]=='-'){
-
-                jogoDaVelha[0][2] = 'o';
-            }
-        } else if (lugar1 == 4) {
-            if(jogoDaVelha[1][0]=='-'){
-
-                jogoDaVelha[1][0] = 'o';
-            }
-        } else if (lugar1 == 5) {
-            if(jogoDaVelha[1][1]=='-'){
-
-                jogoDaVelha[1][1] = 'o';
-            }
-        } else if (lugar1 == 6) {
-            if(jogoDaVelha[1][2]=='-'){
-
-                jogoDaVelha[1][2] = 'o';
-            }
-        } else if (lugar1 == 1) {
-            if(jogoDaVelha[2][0]=='-'){
-
-                jogoDaVelha[2][0] = 'o';
-            }
-        } else if (lugar1 == 2) {
-            if(jogoDaVelha[2][1]=='-'){
-
-                jogoDaVelha[2][1] = 'o';
-            }
-        } else if (lugar1 == 3) {
-            if(jogoDaVelha[2][2]=='-'){
-
-                jogoDaVelha[2][2] = 'o';
-            }
-        }
-        return jogoDaVelha;
-    }
-    public static char[][] l2(int lugar1,char[][] jogoDaVelha){
-        
-        if (lugar1 == 7) {
-            if(jogoDaVelha[0][0]=='-'){
-                jogoDaVelha[0][0] = 'x';
-            }       
-        } else if (lugar1 == 8) {
-            if(jogoDaVelha[0][1]=='-'){
-
-                jogoDaVelha[0][1] = 'x';
-            }
-        } else if (lugar1 == 9) {
-            if(jogoDaVelha[0][2]=='-'){
-
-                jogoDaVelha[0][2] = 'x';
-            }
-        } else if (lugar1 == 4) {
-            if(jogoDaVelha[1][0]=='-'){
-
-                jogoDaVelha[1][0] = 'x';
-            }
-        } else if (lugar1 == 5) {
-            if(jogoDaVelha[1][1]=='-'){
-
-                jogoDaVelha[1][1] = 'x';
-            }
-        } else if (lugar1 == 6) {
-            if(jogoDaVelha[1][2]=='-'){
-
-                jogoDaVelha[1][2] = 'x';
-            }
-        } else if (lugar1 == 1) {
-            if(jogoDaVelha[2][0]=='-'){
-
-                jogoDaVelha[2][0] = 'x';
-            }
-        } else if (lugar1 == 2) {
-            if(jogoDaVelha[2][1]=='-'){
-
-                jogoDaVelha[2][1] = 'x';
-            }
-        } else if (lugar1 == 3) {
-            if(jogoDaVelha[2][2]=='-'){
-
-                jogoDaVelha[2][2] = 'x';
-            }
-        }
-        return jogoDaVelha;
-    }
-    public static int[] comparacao(int[] ganhadores,char[][] jogoDaVelha) {
-        if (jogoDaVelha[0][0] == 'o' && jogoDaVelha[0][1] == 'o' && jogoDaVelha[0][2] == 'o') {
+    public static int[] comparacao(int[] ganhadores,char[][] jogoDaVelha,char y,int z) {
+        if (jogoDaVelha[0][0] == y && jogoDaVelha[0][1] == y && jogoDaVelha[0][2] == y) {
                         ganhadores[2] = 1;
-                        ganhadores[0]++;
-                    } else if (jogoDaVelha[1][0] == 'o' && jogoDaVelha[1][1] == 'o' && jogoDaVelha[1][2] == 'o') {
+                        ganhadores[z]++;
+                    } else if (jogoDaVelha[1][0] == y && jogoDaVelha[1][1] == y && jogoDaVelha[1][2] == y) {
                         ganhadores[2] = 1;
-                        ganhadores[0]++;
-                    } else if (jogoDaVelha[2][0] == 'o' && jogoDaVelha[2][1] == 'o' && jogoDaVelha[2][2] == 'o') {
+                        ganhadores[z]++;
+                    } else if (jogoDaVelha[2][0] == y && jogoDaVelha[2][1] == y && jogoDaVelha[2][2] == y) {
                         ganhadores[2] = 1;
-                        ganhadores[0]++;
-                    } else if (jogoDaVelha[0][0] == 'o' && jogoDaVelha[1][1] == 'o' && jogoDaVelha[2][2] == 'o') {
+                        ganhadores[z]++;
+                    } else if (jogoDaVelha[0][0] == y && jogoDaVelha[1][1] == y && jogoDaVelha[2][2] == y) {
                         ganhadores[2] = 1;
-                        ganhadores[0]++;
-                    } else if (jogoDaVelha[0][2] == 'o' && jogoDaVelha[1][1] == 'o' && jogoDaVelha[2][0] == 'o') {
+                        ganhadores[z]++;
+                    } else if (jogoDaVelha[0][2] == y && jogoDaVelha[1][1] == y && jogoDaVelha[2][0] == y) {
                         ganhadores[2] = 1;
-                        ganhadores[0]++;
-                    } else if (jogoDaVelha[0][0] == 'o' && jogoDaVelha[1][0] == 'o' && jogoDaVelha[2][0] == 'o') {
+                        ganhadores[z]++;
+                    } else if (jogoDaVelha[0][0] == y && jogoDaVelha[1][0] == y && jogoDaVelha[2][0] == y) {
                         ganhadores[2] = 1;
-                        ganhadores[0]++;
-                    } else if (jogoDaVelha[0][1] == 'o' && jogoDaVelha[1][1] == 'o' && jogoDaVelha[2][1] == 'o') {
+                        ganhadores[z]++;
+                    } else if (jogoDaVelha[0][1] == y && jogoDaVelha[1][1] == y && jogoDaVelha[2][1] == y) {
                         ganhadores[2] = 1;
-                        ganhadores[0]++;
-                    } else if (jogoDaVelha[0][2] == 'o' && jogoDaVelha[1][2] == 'o' && jogoDaVelha[2][2] == 'o') {
+                        ganhadores[z]++;
+                    } else if (jogoDaVelha[0][2] == y && jogoDaVelha[1][2] == y && jogoDaVelha[2][2] == y) {
                         ganhadores[2] = 1;
-                        ganhadores[0]++;
-                    }
-
-                    if (jogoDaVelha[0][0] == 'x' && jogoDaVelha[0][1] == 'x' && jogoDaVelha[0][2] == 'x') {
-                            ganhadores[2] = 1;
-                            ganhadores[1]++;
-                        } else if (jogoDaVelha[1][0] == 'x' && jogoDaVelha[1][1] == 'x' && jogoDaVelha[1][2] == 'x') {
-                            ganhadores[2] = 1;
-                            ganhadores[1]++;
-                        } else if (jogoDaVelha[2][0] == 'x' && jogoDaVelha[2][1] == 'x' && jogoDaVelha[2][2] == 'x') {
-                            ganhadores[2] = 1;
-                            ganhadores[1]++;
-                        } else if (jogoDaVelha[0][0] == 'x' && jogoDaVelha[1][1] == 'x' && jogoDaVelha[2][2] == 'x') {
-                            ganhadores[2] = 1;
-                            ganhadores[1]++;
-                        } else if (jogoDaVelha[0][2] == 'x' && jogoDaVelha[1][1] == 'x' && jogoDaVelha[2][0] == 'x') {
-                            ganhadores[2] = 1;
-                            ganhadores[1]++;
-                        } else if (jogoDaVelha[0][0] == 'x' && jogoDaVelha[1][0] == 'x' && jogoDaVelha[2][0] == 'x') {
-                            ganhadores[2] = 1;
-                            ganhadores[1]++;
-                        } else if (jogoDaVelha[0][1] == 'x' && jogoDaVelha[1][1] == 'x' && jogoDaVelha[2][1] == 'x') {
-                            ganhadores[2] = 1;
-                            ganhadores[1]++;
-                        } else if (jogoDaVelha[0][2] == 'x' && jogoDaVelha[1][2] == 'x' && jogoDaVelha[2][2] == 'x') {
-                            ganhadores[2] = 1;
-                            ganhadores[1]++;
-                        } else if (jogoDaVelha[0][0] != '-' && jogoDaVelha[0][1] != '-' && jogoDaVelha[0][2] != '-' &&
+                        ganhadores[z]++;
+                    } else if (jogoDaVelha[0][0] != '-' && jogoDaVelha[0][1] != '-' && jogoDaVelha[0][2] != '-' &&
                                 jogoDaVelha[1][0] != '-' && jogoDaVelha[1][1] != '-' && jogoDaVelha[1][2] != '-' &&
                                 jogoDaVelha[2][0] != '-' && jogoDaVelha[2][1] != '-' && jogoDaVelha[2][2] != '-') {
                             ganhadores[2] = 1;
@@ -166,5 +44,53 @@ public class funcoesTicTacToe{
 
                     return ganhadores;
     }
+    public static char[][] l(int lugar1,char[][] jogoDaVelha,char y){
+        
+        if (lugar1 == 7) {
+            if(jogoDaVelha[0][0]=='-'){
+                jogoDaVelha[0][0] = y;
+            }       
+        } else if (lugar1 == 8) {
+            if(jogoDaVelha[0][1]=='-'){
 
+                jogoDaVelha[0][1] = y;
+            }
+        } else if (lugar1 == 9) {
+            if(jogoDaVelha[0][2]=='-'){
+
+                jogoDaVelha[0][2] = y;
+            }
+        } else if (lugar1 == 4) {
+            if(jogoDaVelha[1][0]=='-'){
+
+                jogoDaVelha[1][0] = y;
+            }
+        } else if (lugar1 == 5) {
+            if(jogoDaVelha[1][1]=='-'){
+
+                jogoDaVelha[1][1] = y;
+            }
+        } else if (lugar1 == 6) {
+            if(jogoDaVelha[1][2]=='-'){
+
+                jogoDaVelha[1][2] = y;
+            }
+        } else if (lugar1 == 1) {
+            if(jogoDaVelha[2][0]=='-'){
+
+                jogoDaVelha[2][0] = y;
+            }
+        } else if (lugar1 == 2) {
+            if(jogoDaVelha[2][1]=='-'){
+
+                jogoDaVelha[2][1] = y;
+            }
+        } else if (lugar1 == 3) {
+            if(jogoDaVelha[2][2]=='-'){
+
+                jogoDaVelha[2][2] = y;
+            }
+        }
+        return jogoDaVelha;
+    }
 }
--- ticTacToe.java
+++ ticTacToe.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class ticTacToe {
 
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         char[][] jogoDaVelha = new char[3][3];
-        int primeiro;
-        int[] ganhadores=new int[3];
+        char x = 'x', o = 'o';
+        int primeiro = 0, um = 0, dois = 1;
+        String jogador[] = { "jogador 1 - O", "jogador 2 - X" };
+        int[] ganhadores = new int[3];
 
-        while (ganhadores[0] < 2 && ganhadores[1] < 2) {
+        JOptionPane.showMessageDialog(null, "O jogo Funciona com base no teclado numerico", "Forma do jogo",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        primeiro = JOptionPane.showOptionDialog(null, " escolha quem vai jogar primeiro: ", "escolha",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null, jogador, jogador[0]);
+
+        while (ganhadores[0] < 2 && ganhadores[1] < 2 && primeiro != -1) {
 
             for (int l = 0; l < 3; l++) {
                 for (int c = 0; c < 3; c++) {
@@ -17,72 +27,95 @@ public class ticTacToe {
                 }
             }
 
-            System.out.print(" 1 = o - Jogador 1\n" +
-                    " 2 = x - jogador 2\n" +
-                    " escolha quem vai jogar primeiro: ");
-            primeiro = sc.nextInt();
-
             System.out.println("");
 
             funcoesTicTacToe.tabela(jogoDaVelha);
 
-            if (primeiro == 1) {
+            if (primeiro == 0) {
                 ganhadores[2] = 0;
                 while (ganhadores[2] == 0) {
-                    
-                    System.out.print("onde deseja inserir jogador 1: ");
-                    funcoesTicTacToe.l1( sc.nextInt(),jogoDaVelha);
+
+                    funcoesTicTacToe.l(Integer.parseInt(JOptionPane.showInputDialog("onde deseja inserir jogador 1: ")),
+                            jogoDaVelha, o);
                     System.out.println("");
-                    
+
                     funcoesTicTacToe.tabela(jogoDaVelha);
 
-                    funcoesTicTacToe.comparacao(ganhadores,jogoDaVelha);
-                   
-                    if (ganhadores[2] != 1) {
+                    funcoesTicTacToe.comparacao(ganhadores, jogoDaVelha, o, um);
 
-                        System.out.print("onde deseja inserir jogador 2: ");
-                        funcoesTicTacToe.l2(sc.nextInt(),jogoDaVelha);
-                        System.out.println("");
-                      
-                        funcoesTicTacToe.tabela(jogoDaVelha);
-
-                        funcoesTicTacToe.comparacao(ganhadores,jogoDaVelha);
+                    if (ganhadores[2] == 1) {
+                        if (ganhadores[0] == 1) {
+                            JOptionPane.showMessageDialog(null, "Ganhador é...\nVencedor 1");
+                        }
                     }
+                    if (ganhadores[2] != 1) {
+
+                        funcoesTicTacToe.l(
+                                Integer.parseInt(JOptionPane.showInputDialog("onde deseja inserir jogador 2: ")),
+                                jogoDaVelha, x);
+                        System.out.println("");
+
+                        funcoesTicTacToe.tabela(jogoDaVelha);
+
+                        funcoesTicTacToe.comparacao(ganhadores, jogoDaVelha, x, dois);
+                        if (ganhadores[2] == 1) {
+                            if (ganhadores[1] == 1) {
+                                JOptionPane.showMessageDialog(null, "Ganhador é...\nVencedor 2");
+                            }
+
+                        }
+                    }
+
                 }
-            } else if (primeiro == 2) {
+            } else if (primeiro == 1) {
+
                 ganhadores[2] = 0;
                 while (ganhadores[2] == 0) {
-                    
-                    System.out.print("onde deseja inserir jogador 2: ");
-                    funcoesTicTacToe.l2(sc.nextInt(),jogoDaVelha);
+
+                    funcoesTicTacToe.l(Integer.parseInt(JOptionPane.showInputDialog("onde deseja inserir jogador 2: ")),
+                            jogoDaVelha, x);
                     System.out.println("");
-                    
+
                     funcoesTicTacToe.tabela(jogoDaVelha);
 
-                    funcoesTicTacToe.comparacao(ganhadores,jogoDaVelha);
+                    funcoesTicTacToe.comparacao(ganhadores, jogoDaVelha, x, dois);
+                    if (ganhadores[2] == 1) {
+                        if (ganhadores[2] == 1) {
+                            JOptionPane.showMessageDialog(null, "Ganhador é...\nVencedor 2");
+                        }
+                    }
 
                     if (ganhadores[2] != 1) {
 
-                        System.out.print("onde deseja inserir jogador 1: ");
-                        funcoesTicTacToe.l1( sc.nextInt(),jogoDaVelha);
+                        funcoesTicTacToe.l(
+                                Integer.parseInt(JOptionPane.showInputDialog("onde deseja inserir jogador 1: ")),
+                                jogoDaVelha, o);
                         System.out.println("");
-                        
+
                         funcoesTicTacToe.tabela(jogoDaVelha);
-                        
-                        funcoesTicTacToe.comparacao(ganhadores,jogoDaVelha);
+
+                        funcoesTicTacToe.comparacao(ganhadores, jogoDaVelha, o, um);
+                        if (ganhadores[2] == 1) {
+                            if (ganhadores[1] == 1) {
+                                JOptionPane.showMessageDialog(null, "Ganhador é...\nVencedor 1");
+                            }
+                        }
                     }
                 }
             }
 
         }
+
+        if (primeiro == -1) {
+            JOptionPane.showMessageDialog(null, "Fim do programa...", null, JOptionPane.INFORMATION_MESSAGE);
+        }
         if (ganhadores[0] == 2) {
-            System.out.println("==================================");
-            System.out.println("O viToRiosO foi UM 11111111 !!!!!!");
-            System.out.println("==================================");
-        } else {
-            System.out.println("==================================");
-            System.out.println("O viToRiosO foi DOIS 22222222 !!!!!!");
-            System.out.println("==================================");
+            JOptionPane.showMessageDialog(null, "==================================\n" +
+                    "O viToRiosO foi UM 11111111 !!!!!!\n" +
+                    "==================================");
+        } else if (ganhadores[1] == 2) {
+            JOptionPane.showMessageDialog(null, "==================================\n"
+                    + "O viToRiosO foi Dois 22222222 !!!!!!\n" + "==================================");
         }
     }
 }
